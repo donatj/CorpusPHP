56388e1c2afe5e4543d3ac030845ed8c7bf1163b
Jesse Donat <donatj@gmail.com>
2010-02-11 12:20:38 -0600

    Efficiency and scaling update to the thumbnailer.

--- images/displayImage.php
+++ images/displayImage.php
@@ -1,11 +1,13 @@
 <?
-/*
-* * Damned Simple Image Scaler * *
-* By Jesse G. Donat
-* March 25, 2009
+/**
+* @name Damned Simple Image Scaler
+* @author Jesse G. Donat
+* 
+* Created: March 25, 2009
+* Updated: February 11 2010
+* 
 */
 error_reporting( E_ALL ^ E_NOTICE );
-$s_time = microtime(true);
 
 if( !file_exists($_GET['src']) ) {
 	header("HTTP/1.0 404 Not Found");
@@ -15,9 +17,14 @@ if( !file_exists($_GET['src']) ) {
 $orig = getimagesize( $_GET['src'] );
 
 if( $_GET['w'] > 0 && $_GET['h'] > 0 ) {
-	$w = (int)$_GET['w'];
-	$h = (int)$_GET['h'];
-}elseif( $_GET['w'] > 0 ) {
+	if( $_GET['w'] / $_GET['h'] > $orig[0] / $orig[1] ) {
+		unset($_GET['w']); //limit by height
+	}else{
+		unset($_GET['h']); //limit by width
+	}
+}
+
+if( $_GET['w'] > 0 ) {
 	$w = (int)$_GET['w'];
 	$h = ceil( ( $_GET['w'] / $orig[0]) * $orig[1] );
 }elseif($_GET['h'] > 0) {
@@ -33,15 +40,15 @@ $cacheName = '../cache/' . md5( json_encode($cacheHash) ) . '.thumb.jpg';
 
 if( !file_exists($cacheName) ) {
 	$sourceFile = imagecreatefromstring( file_get_contents( $_GET['src'] ) ); //doesn't care about type
-	$newImage = imagecreatetruecolor($w,$h);
+	if( imageistruecolor( $sourceFile ) ) {
+		$newImage = imagecreatetruecolor($w,$h);
+	}else{
+		$newImage = imagecreate($w,$h);
+	}
+	
 	imagecopyresampled($newImage, $sourceFile, 0, 0, 0, 0, $w, $h, $orig[0], $orig[1]);
 	imagejpeg($newImage, $cacheName, 90);
 }
 
-$fp = fopen($cacheName, 'rb');
-header("Last-Modified: ".gmdate( "D, d M Y H:i:s", filemtime($cacheName) )." GMT");
-header("OrigHeight: " . (int)$orig[0]);
-header("OrigWidth: " . (int)$orig[1]);
-header('Content-type: image/jpeg');
-header('X-Time-Taken: ' . microtime(true) - $s_time);
-fpassthru($fp);
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: " . $cacheName);
