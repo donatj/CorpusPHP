00c3bf4d077d67d3126b4a773d7c732656d9d6f7
Jesse Donat <j.donat@capstonedigital.com>
2013-04-10 13:49:09 -0500

    Wait, nope.  $_database should be the connection info

--- Source/Application/databases/db.php
+++ Source/Application/databases/db.php
@@ -5,8 +5,7 @@ class db extends Database {
 	protected static $_host     = DB_HOST;
 	protected static $_user     = DB_USER;
 	protected static $_password = DB_PASSWORD;
+	protected static $_database = DB_DATABASE;
+
 	protected static $_charset  = 'utf8';
-
-	protected static $_database;
-
 }
