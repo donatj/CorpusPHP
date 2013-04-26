bbb427f5eb48b6dffd45831e488612ace9013d73
Jesse Donat <donatj@gmail.com>
2013-04-26 11:41:20 -0500

    Corrections to the Github api modules pulled in

--- Source/corpus/modules/Github/feed.php
+++ Source/corpus/modules/Github/feed.php
@@ -17,13 +17,15 @@ $_config->defaults(
 $USERNAME   = firstNotEmpty( $data['username'], $_config->USERNAME );
 $REPOSITORY = firstNotEmpty( $data['repository'], $_config->REPOSITORY );
 
-$feedUrl = "http://github.com/api/v1/json/{$USERNAME}/{$REPOSITORY}/commits/master";
+$opts = array( 'http' => array('header'  => 'User-Agent: Github Sucks at APIs') );
+$context = stream_context_create($opts);
+$feedUrl = "https://api.github.com/repos/{$USERNAME}/{$REPOSITORY}/commits";
 $cacheKey = md5($feedUrl);
 
 if( !$_cache->isExpired( $cacheKey ) ) {
 	$feed = $_cache->$cacheKey;
 }else{
-	if( $feed = file_get_contents( $feedUrl ) ) {
+	if( $feed = file_get_contents( $feedUrl, false, $context ) ) {
 		$_cache->set( $cacheKey, $feed, 4, 'HOUR', false );
 	}else{
 		$feed = $_cache->$cacheKey;
@@ -33,15 +35,23 @@ if( !$_cache->isExpired( $cacheKey ) ) {
 
 $github = json_decode($feed, true);
 
+
 echo '<div class="'. $_config->DIVCLASS .'"><h2>Recent Activity</h2>';
-foreach( $github['commits'] as $commit ) {
-	$date = date($_config->DATEFORMAT, strtotime($commit['committed_date']));
+foreach( $github as $commit_full ) {
+	$commit = $commit_full['commit'];
+	$commit_date = $commit['committer']['date'];
+	$date = date($_config->DATEFORMAT, strtotime( $commit_date ));
 	if( $date != $dateHold ) {
-		echo '<h3>' . co::module('blog/date', strtotime($commit['committed_date']) ) . '</h3>';
+		echo '<h3>' . co::module('blog/date', strtotime( $commit_date ) ) . '</h3>';
+	}
+	if( $commit_full['committer']['id'] != $authorHold ) {
+		echo '<h4>' . $commit['committer']['name'] . ' <small>(<a href="https://github.com/'.$commit_full['committer']['login'].'" target="_blank">'. $commit_full['committer']['login'] .'</a>)</small></h4>';
 	}
 	echo nl2br( str_replace( "\t", '&nbsp;&nbsp;', htmlE($commit['message']) ) );
 	echo '<hr />';
 	$dateHold = $date;
+	//drop($commit_full['committer']);
+	$authorHold = $commit_full['committer']['id'];
 }
 echo '</div>';
 
